0bc75a28f3236d38a0d331b9d3b46825449b6db6
hiofsam <43729313+hiofsam@users.noreply.github.com>
2018-10-02 17:23:33 +0200

    Revert "Revert "indeks ferdig""
    This reverts commit 6a1b7731bad5cbd38b116257d1c586d22b519ff3.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+window.onload = oppstart;
+
+function oppstart() {
+    document.getElementById("sokGit").onclick = sok;
+}
+
+function sok(){
+    window.open("https://www.google.no/search?q=github&oq=github+&aqs=chrome..69i57j69i60l2j0l3.4498j0j4&sourceid=chrome&ie=UTF-8");
+}
